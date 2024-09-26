f0e9ed8b9b65046e5047b78d1607708b3520da05
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2024-09-26 22:33:39 +0200

    started testing and fixed bugs

--- packages/foundry/test/LotteryTest.t.sol
+++ packages/foundry/test/LotteryTest.t.sol
@@ -1,7 +1,9 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "../contracts/Lottery.sol";
+import "../script/Deploy.s.sol";
 
 contract LotteryTest is Test {}
